7121bb0db556d0e63a14c36eab116796d2a5e3c3
annae10 <63148588+annae10@users.noreply.github.com>
2023-08-29 21:13:17 +0300

    Update main.cpp
    libcurl and htmlcxx

--- main.cpp
+++ main.cpp
@@ -1,27 +1,70 @@
-﻿#define CURL_STATICLIB
-#include <iostream>
+#define CURL_STATICLIB
 #include <curl/curl.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <htmlcxx/html/ParserDom.h>
 
+using namespace std;
+using namespace htmlcxx;
 
-static size_t getResponsetoString(void* contents, size_t size, size_t nmemb, void* userp)
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+	((string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
 }
 
-int main() {
-  
-    CURL* curl;
-    CURLcode response;
-    std::string str_response;
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.museumsassociation.org/museums-journal/news/#");
-    //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, getResponsetoString);
-    //curl_easy_setopt(curl, CURLOPT_WRITEDATA, &str_response);
-    response = curl_easy_perform(curl);
-    std::cout << response << "\n";
-    curl_easy_cleanup(curl);
+int main()
+{
+	HTML::ParserDom parser;
+	string url = "https://www.veranda.com/travel/g1436/best-museums-in-the-world/";
+	string page;
 
-    return 0;
+    //load page into variable page
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	CURL* curl = curl_easy_init();
+	if (curl)
+    {
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &page);
+
+		CURLcode res = curl_easy_perform(curl);
+		if (res == CURLE_OK)
+		{
+			//cout<< page <<endl;
+		}
+		else
+		{
+			cerr<<"Error: "<<curl_easy_strerror(res)<<endl;
+		}
+		curl_easy_cleanup(curl);
+	}
+	curl_global_cleanup();
+
+    //page parsing
+	tree<HTML::Node> dom = parser.parseTree(page);
+	vector<string>titles;
+	for(auto it = dom.begin();it != dom.end(); ++it)
+	{
+		if(it->tagName() == "h2" )
+		{
+			string title = "";
+			++it;
+			while (it != dom.end() && !it->isTag())
+			{
+				title += it->text();
+				++it;
+			}	
+			titles.push_back(title);
+		}
+	}
+
+	for (const auto& title: titles)
+	{
+        if (title != ""){
+		cout<<"Title: "<<title<<endl;
+        }
+	}	
+	return 0;
 }
-
